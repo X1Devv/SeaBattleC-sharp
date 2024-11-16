23558657f70277c7b6725e517b497b90df1b3fa2
X1devv <rommal563@gmail.com>
2024-11-16 11:02:39 +0200

    version 0.1 | add backend

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Logic.StartGame();
+        bool SwitchMainField = true;
+
+        while (true)
+        {
+            Console.Clear();
+
+            if (SwitchMainField)
+            {
+                Console.WriteLine("Your");
+                Logic.DrawField(Logic.Width, Logic.Hight, Logic.FieldMain);
+            }
+            else
+            {
+                Console.WriteLine("Enemy");
+                Logic.DrawField(Logic.Width, Logic.Hight, Logic.FieldEnemy);
+            }
+
+            Console.WriteLine("Write Cords:(a-j 1-10)");
+            var CoordinatesPLayer = Console.ReadLine();
+
+            Logic.KillShips(SwitchMainField ? Logic.FieldMain : Logic.FieldEnemy, CoordinatesPLayer);
+            SwitchMainField = !SwitchMainField;
+        }
+    }
+}
